f95995c7533a75da8bf66358b1d95fac97ebaea3
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-01-26 07:00:43 +0200

    added the update functionality to the products, I have totally changed the approach with my mongoose model to get it to work in a simpler way

--- app/models/product.js
+++ app/models/product.js
@@ -1,11 +1,58 @@
 var mongoose = require('mongoose');
-var	Schema = mongoose.Schema;
 
-var ProductSchema = new Schema({
-	name: String,
-	description: String,
-	price: Number,
-	image: String
+// Defining Product Schema
+var productSchema = mongoose.Schema({
+
+	name:{
+		type: String,
+		required: true
+	},
+	description:{
+		type: String,
+		required: true
+	},
+	price:{
+		type: Number,
+		required: true
+	},
+	image:{
+		type: String,
+		required: true
+	},
+	created_at:{
+		type: Date,
+		default: Date.now
+	}
 });
 
-module.exports = mongoose.model('Product', ProductSchema);
+var Product = module.exports = mongoose.model('Product', productSchema);
+
+// Get All Products
+
+module.exports.getProducts = function(callback, limit){
+	Product.find(callback).limit(limit);
+}
+
+// Get a Product
+
+module.exports.getProductById = function(id, callback){
+	Product.findById(id, callback);
+}
+
+// Add a Product
+
+module.exports.addProduct = function(product, callback){
+	Product.create(product, callback);
+}
+
+// Update a product
+module.exports.updateProduct = function(id, product, options, callback){
+	var query = {_id: id};
+	var update = {
+		name: product.name,
+		description: product.description,
+		price: product.price,
+		description: product.image
+	}
+	Product.findOneAndUpdate(query, update, options,callback);
+}
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-
+ 
 //========== BASE SETUP
 
 // calling needed packages
 
 var express = require('express');
-		app = express();
-		bodyParser = require('body-parser');
-		mongoose = require('mongoose');
+var	app = express();
+var	bodyParser = require('body-parser');
+var	mongoose = require('mongoose');
+
+Product = require('./app/models/product');
 
 // Connect to Mongoose
 mongoose.connect('mongodb://localhost/edealzapi');
-
-var Product = require('./app/models/product');
 var db = mongoose.connection;
+
 
 
 // configure app to use bodyParser()
@@ -21,118 +22,58 @@ var db = mongoose.connection;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 
-
-// setting the port for our app
-
-var port = process.env.PORT || 4000;
-
-
-//========== ROUTES FOR MY API
-// getting an instance for my app
-
-var router = express.Router();
-
-// middleware to use for all requests
-router.use(function(req, res, next){
-	// do logging
-	console.log('Something is happening');
-	next(); // making usre we go to the next route and not stop there
+// test route on root
+app.get('/', function (req, res) {
+  res.send('Please use /api/products...');
 });
 
-// test route
-router.get('/', function (req, res) {
-  res.json({ message: 'Hooray! I did it again...'});
-})
-
-
-// RESTful routes here
-// on routes that end in /products
-
-router.route('/products')
-
-    // create a product (accessed at POST http://localhost:4000/api/products)
-    .post(function(req, res) {
-        
-        var product = new Product();      // create a new instance of the Product model
-        product.name = req.body.name;  // set the products name (comes from the request)
-        product.description = req.body.description;  // set the products description (comes from the request)
-        product.price = req.body.price;  // set the products price (comes from the request)
-        product.image = req.body.image;  // set the products image (comes from the request)
-
-        // save the product and check for errors
-        product.save(function(err) {
-            if (err)
-                res.send(err);
-            res.json({ message: 'Product created!' });
-        });
-        
-    })
-
-    // get all the products (accessed at GET http://localhost:4000/api/products)
-    .get(function(req, res) {
-    	Product.find(function(err, products) {
-            if (err)
-                res.send(err);
-
-            res.json(products);
-        });
+// Get all the products
+app.get('/api/products', function(req, res){
+    Product.getProducts(function(err, products){
+        if(err){
+            throw err;
+        }
+        res.json(products);
     });
+});
 
-// on routes that end in /products/:product_id
-router.route('/products/:product_id')
-		// getting the product (accessed at GET http://localhost:4000/api/products/:product_id)
-		.get(function(req, res) {
-		  Product.findById(req.params.product_id, function(err, product){
-		  			if (err)
-		  				res.send(err);
-		  			res.json(product);
-		  });
-		});
-
-		// updating the product (accessed at PATCH http://localhost:4000/api/products/:product_id)
-    router.put(function(req, res) {
-        // use our product model to find the product we want
-        Product.findById(req.params.product_id, function(err, product) {
-
-            if (err)
-                res.send(err);
-
-            product.name = req.body.name;  // update the products info
-            product.description = req.body.description;
-            product.price = req.body.price;
-            product.image = req.body.image;
-
-            // save the product
-            product.save(function(err) {
-                if (err)
-                    res.send(err);
-                res.json({ message: 'Product updated!' });
-            });
-
-        });
+// Get a product
+app.get('/api/products/:_id', function(req, res){
+    Product.getProductById(req.params._id, function(err, product){
+        if(err){
+            throw err;
+        }
+        res.json(product);
     });
+});
 
-    // delete the product with this id (accessed at DELETE http://localhost:8080/api/bears/:product_id)
-    router.delete(function(req, res) {
-        Product.remove({
-            _id: req.params.product_id
-        }, function(err, product) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'Successfully deleted' });
-        });
+// Get a product
+app.post('/api/products', function(req, res){
+    var product = req.body;
+    Product.addProduct(product, function(err, product){
+        if(err){
+            throw err;
+        }
+        res.json(product);
     });
+});
 
-//========== REGISTER ALL ROUTES
-// adding routes prefixer at /api
-
-app.use('/api', router);
+// Update a product
+app.put('/api/products/:_id', function(req, res){
+    var id = req.params._id;
+    var product = req.body;
+    Product.updateProduct(id, product, {}, function(err, product){
+        if(err){
+            throw err;
+        }
+        res.json(product);
+    });
+});
 
 //========== START THE SERVER
 
-app.listen(port);
-console.log('eDealz is up and listening on port ' + port + '...');
+app.listen(3000);
+    console.log('eDealz is up and listening on port 3000...');
 
 
 
